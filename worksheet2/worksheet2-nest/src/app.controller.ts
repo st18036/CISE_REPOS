ae4f18617c22586056757eb3eca3c419d126dbfc
Chetan Dev <chetandev48@gmail.com>
2024-07-24 18:36:51 +1200

    Api Endpoints + Articles + dotenv
    created api endpoints using dummy data(articles)
    
    created a dotenv file
    
    used the end point and made changes to app.controller.ts file

--- worksheet2/worksheet2-nest/src/app.controller.ts
+++ worksheet2/worksheet2-nest/src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ARTICLES } from './dummydata/articles';
+
 
 @Controller()
 export class AppController {
@@ -9,4 +12,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+    @Get('/api/articles')
+    getArticles(): any[] {
+        return ARTICLES;
+    }
+
+    @Get('/api/articles/:id')
+    getArticlesByID(@Param('id') id: string): any[] {
+    return ARTICLES.find((n) => n._id === id);
+    }
 }
