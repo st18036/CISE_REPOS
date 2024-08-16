803a20256d94480e221d58b24e04c2bdd730efd6
Chetan Dev <chetandev48@gmail.com>
2024-08-16 18:36:10 +1200

    backend created
    apis built
    
    server setup with mongodb

---
+++ worksheet3/worksheet3_backend/src/api/books/create-book.dto.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Date } from 'mongoose';
+
+export class CreateBookDto {
+  title: string;
+  isbn: string;
+  author: string;
+  description: string;
+  published_date: Date;
+  publisher: string;
+  updated_date: Date;
+}
